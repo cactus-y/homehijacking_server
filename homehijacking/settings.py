c3b737878d869da1638f279dce02179f057357a2
2019032160 <2019032160@hanyang.ac.kr>
2021-01-19 13:49:39 +0900

    room, rsv edit

--- homehijacking/settings.py
+++ homehijacking/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # apps
     'users',
     'room',
+    'reservation',
 
     # packages
     'rest_framework',
--- reservation/models.py
+++ reservation/models.py
@@ -3,6 +3,10 @@ from users.models import User
 
 # Create your models here.
 class Reservation(models.Model):
-    host = models.ForeignKey(User, on_delete=models.CASCADE)
-    guest = models.ForeignKey(User, on_delete=models.CASCADE)
-    rsv_date = models.CharField(max_length=12)
+    name = models.CharField(max_length=30, blank=True)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='host')
+    guest = models.ForeignKey(User, on_delete=models.CASCADE, related_name='guest')
+    rsv_date = models.CharField(max_length=17)
+
+    def __str__(self):
+        return self.name
