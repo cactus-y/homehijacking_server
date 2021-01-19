90c36b1911718611a60a865d249beb6b288f8619
2019032160 <2019032160@hanyang.ac.kr>
2021-01-19 13:15:34 +0900

    room model

--- room/models.py
+++ room/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
-# class Room(models.Model):
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    guest = models.ForeignKey(User, on_delete=models.CASCADE)
+    room_image = models.ImageField(upload_to='room_image/%Y/%m/%d/', blank=True)
+    max_guest = models.PositiveSmallIntegerField(default=1)
+    memo = models.TextField(blank=True)
