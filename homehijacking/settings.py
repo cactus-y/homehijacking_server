97d525c9055f149b2aa08584e8b78dd3e9509af6
2019032160 <2019032160@hanyang.ac.kr>
2021-01-23 07:25:30 +0900

    allowed host

--- homehijacking/settings.py
+++ homehijacking/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'qj1*jtwl1+np#kzx9_lbn!=7=8t$a7-m2+vb26a*=lo9ypf(*#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.125.95.186', '127.0.0.1']
 
 
 # Application definition
